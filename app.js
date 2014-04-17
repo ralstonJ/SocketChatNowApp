e6c0e09f512e0097ff84a4e01b9587e40aac21a7
Ralston J. Almeida <ralstonalmeida@gmail.com>
2014-04-17 02:24:40 -0400

    Second Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var fs = require('fs'), http = require('http') , socketio = require('socket.io');
+
+var server = http.createServer(function(req, res) {
+
+	res.writeHead(200, { 'Content-type': 'text/html'});
+	
+	res.end(fs.readFileSync(__dirname + '/speechApp.html'));
+
+}).listen(process.env.PORT, function() {console.log('Listening at:' + process.env.PORT);});
+ 
+ 
+socketio.listen(server).on('connection', function (socket) {
+	
+	socket.on('message', function (msg) {
+		console.log('Message Received: ', msg);
+		
+		//Broadcast to all clients
+		socket.broadcast.emit('message', msg);
+	});
+
+});
